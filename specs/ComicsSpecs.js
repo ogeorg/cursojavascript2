2cb161097b4e91c5fc4c7f05a85b0b110acc628a
Guille <ggalmazor@gmail.com>
2015-02-27 14:33:13 +0100

    Añadido test de modelo

--- specs/ComicsSpecs.js
+++ specs/ComicsSpecs.js
@@ -10,7 +10,34 @@ describe("Las colecciones de Comics", function () {
     expect(comics.contains(containedComic)).toBeTruthy();
     expect(comics.contains(notContainerComic)).toBeFalsy();
   });
-  it("Saben obtener la intersección con otra colección", function () {
-    expect(true).toBeFalsy();
+  describe("Saben obtener la intersección con otra colección", function () {
+    it("cuando ambas están vacías", function () {
+      var comics1 = new marvel.model.Comics([]);
+      var comics2 = new marvel.model.Comics([]);
+      expect(comics1.intersection(comics2).length).toBe(0);
+    });
+
+    it("cuando una está vacía", function () {
+      var comics1 = new marvel.model.Comics([createComic()]);
+      var comics2 = new marvel.model.Comics([]);
+      expect(comics1.intersection(comics2).length).toBe(0);
+    });
+
+    it("cuando no hay comics en común", function () {
+      var comics1 = new marvel.model.Comics([createComic()]);
+      var comics2 = new marvel.model.Comics([createComic()]);
+      expect(comics1.intersection(comics2).length).toBe(0);
+    });
+
+    it("cuando hay comics en común", function () {
+      var commonComic = createComic();
+      var comics1 = new marvel.model.Comics([createComic(), commonComic]);
+      var comics2 = new marvel.model.Comics([commonComic, createComic()]);
+
+      var intersection = comics1.intersection(comics2);
+
+      expect(intersection.length).toBe(1);
+      expect(intersection[0]).toBe(commonComic);
+    });
   });
 });
--- specs/ExampleSpecs.js
+++
@@ -1,5 +0,0 @@
-describe("Este es un ejemplo", function() {
-  it("de cómo hacer un test con Jasmine y Karma", function() {
-    expect(1).toBe(2);
-  });
-});
