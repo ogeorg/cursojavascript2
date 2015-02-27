320ad02b389edb8a5ade8390e1b71e70ecf7aa01
Guille <ggalmazor@gmail.com>
2015-02-27 12:59:26 +0100

    Fin del paso 1

--- src/js/match-finder.js
+++ src/js/match-finder.js
@@ -1,6 +1,35 @@
 $(document).on('ready', function () {
 
-  // Ejemplo de cómo meter un <option> al primer <select>
-  $('#personaje1').append("<option value=\"1234\">Súper López</option>");
+  var characters = api.characters();
+  for (var i = 0; i < characters.length; i++) {
+    $('#personaje1').append("<option value=\"" + characters[i].id + "\">" + characters[i].name + "</option>");
+    $('#personaje2').append("<option value=\"" + characters[i].id + "\">" + characters[i].name + "</option>");
+  }
+
+  $('#buscador').on('submit', function (e) {
+    e.preventDefault();
+    $('#resultados tbody').html("");
+
+    if ($('#personaje1').val() === $('#personaje2').val())
+      throw Error("Los personajes son iguales");
+
+    api.comics($('#personaje1').val(), function (comics1) {
+      api.comics($('#personaje2').val(), function (comics2) {
+        var ambos = [];
+
+        for (var i = 0; i < comics1.length; i++) {
+          for (var j = 0; j < comics2.length; j++) {
+            if (comics1[i].id === comics2[j].id) {
+              ambos.push(comics1[i]);
+            }
+          }
+        }
+
+        for (var i = 0; i < ambos.length; i++) {
+          $('#resultados tbody').append('<tr><td>' + ambos[i].id + '</td><td>' + ambos[i].title + '</td><td>' + ambos[i].characters + '</td></li>');
+        }
+      });
+    });
+  });
 
 });
